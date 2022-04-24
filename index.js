c42f04a9725abe0cfdcea5e393fc1cc72c094a34
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-04-24 18:14:18 +0600

    post delete and get added

--- index.js
+++ index.js
@@ -2,28 +2,69 @@ const express = require('express')
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const {
+    MongoClient,
+    ServerApiVersion,
+    ObjectId
+} = require('mongodb');
 
 // middlewire
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
-app.get('/',(req,res) => {
-    res.send('Running Genius server')
-})
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bnf0w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log('connected')
-  client.close();
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
 });
 
 
-app.listen(port,() => {
+const run = async () => {
+    try {
+        await client.connect()
+        const serviceCollection = client.db('GeniusCar').collection('services')
+        app.get('/service', async (req, res) => {
+            const query = req.query
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+
+        //find a single user
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params
+            const query = {
+                _id: ObjectId(id)
+            }
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+
+        //post services
+        app.post('/service',async (req,res) => {
+            const service = req.body
+            const result = await serviceCollection.insertOne(service)
+            res.send(result)
+        })
+
+        //delete service
+        app.delete('/service/:id',async (req,res) => {
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            console.log(query)
+            const result = await serviceCollection.deleteOne(query)
+            res.send(result);
+        })
+
+    } finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+app.listen(port, () => {
     console.log('Listening to port' + port)
 })
