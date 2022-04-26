2d166364df2a7b633ee2853af0b2e1c72242d560
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-04-26 18:10:23 +0600

    json token verify added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000
+const jwt = require('jsonwebtoken');
 const {
     MongoClient,
     ServerApiVersion,
@@ -20,11 +21,40 @@ const client = new MongoClient(uri, {
     serverApi: ServerApiVersion.v1
 });
 
+// middleware
+const verifyToken = (req, res, next) => {
+    const AuthHeader = req?.headers?.authorization
+    if(!AuthHeader){
+        return res.send({message: 'unauthorized access'})
+    }
+    const token = AuthHeader?.split(' ')[1]
+    jwt.verify(token,process.env.DB_ACCESS_TOKEN,(err,decoded) => {
+        if(err){
+            return res.status(403).send({message: 'forbidden access'});
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 const run = async () => {
     try {
         await client.connect()
         const serviceCollection = client.db('GeniusCar').collection('services')
+        const orderCollection = client.db('GeniusCar').collection('order')
+
+        // Auth
+        app.post('/login', async (req, res) => {
+            const email = req.body
+            const accessToken = jwt.sign(email, process.env.DB_ACCESS_TOKEN, {
+                expiresIn: '1d'
+            })
+            res.send({
+                accessToken
+            })
+        })
+
+        //services api
         app.get('/service', async (req, res) => {
             const query = req.query
             const cursor = serviceCollection.find(query)
@@ -43,19 +73,44 @@ const run = async () => {
         })
 
         //post services
-        app.post('/service',async (req,res) => {
+        app.post('/service', async (req, res) => {
             const service = req.body
             const result = await serviceCollection.insertOne(service)
             res.send(result)
         })
 
         //delete service
-        app.delete('/service/:id',async (req,res) => {
+        app.delete('/service/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id: ObjectId(id)}
+            const query = {
+                _id: ObjectId(id)
+            }
             console.log(query)
             const result = await serviceCollection.deleteOne(query)
             res.send(result);
+        })
+
+        //order collection insert api
+        app.post('/order', async (req, res) => {
+            const order = req.body
+            const result = await orderCollection.insertOne(order)
+            res.send(result)
+        })
+
+        //get the order collection
+        app.get('/order', verifyToken, async (req, res) => {
+            const decoded = req?.decoded?.email
+            const email = req.query.email
+            console.log(decoded)
+            if(decoded === email){
+                const query = {email}
+                const cursor = orderCollection.find(query)
+                const result = await cursor.toArray()
+                res.send(result)
+            }
+            else{
+                res.status(403).send({message: 'forbidden access'})
+            }
         })
 
     } finally {
